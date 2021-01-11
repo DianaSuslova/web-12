0e10be029fee8d43ad2db65926c20d04503d48b8
DianaSuslova <1680793@mail.ru>
2021-01-11 17:29:54 +0300

    вроде получается

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 let App = {
   data() {
     return {
+      fam :'',
+      name:'',
+      students: []
     };
   },
   methods: {
+    famEnter() {
+      this.fam = event.target.value;
+      console.log(this.fam);
+    },
+    nameEnter() {
+      this.name = event.target.value;
+      console.log(this.name);
+    },
+    Sabmit() {
 
+    }
   }
 };
-Vue.createApp(App).mount('#a');
+Vue.createApp(App).mount('#app');
